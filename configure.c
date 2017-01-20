857066f328817d75d2976a9ddb65cf3ab5ffbe29
Daniel Bevenius <daniel.bevenius@gmail.com>
2017-01-20 08:01:47 +0100

    fix spelling in comment

--- configure.c
+++ configure.c
@@ -10,7 +10,7 @@
 int main() {
     uv_loop_t* loop = uv_default_loop();
     // currently the only signal that is implemented is SIGPROF which is a signal
-    // the kernel generates this signal upon the expiration of a profiling timer set by a call to setitimer()
+    // the kernel generates this signal upon the expiration of a profiling timer set by a call to settimer()
     int r = uv_loop_configure(loop, UV_LOOP_BLOCK_SIGNAL, SIGPROF);
     printf("uv_loop_configure returned %d\n", r);
     uv_run(loop, UV_RUN_DEFAULT);
